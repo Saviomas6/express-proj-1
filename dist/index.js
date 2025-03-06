c1f7cd98b976b390ed80b71f4e6a884724511c95
Savio Mascarenhas <savio.m@adsmn.in>
2025-03-06 17:12:23 +0530

    minor changes

--- dist/index.js
+++ dist/index.js
@@ -3,15 +3,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// src/index.ts
 const express_1 = __importDefault(require("express"));
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
+const cors_1 = __importDefault(require("cors"));
+require("dotenv/config");
+const config_1 = require("./db/config");
+const userRouter_1 = __importDefault(require("./routes/userRouter"));
 const app = (0, express_1.default)();
-const port = process.env.PORT || 5000;
-app.get("/", (req, res) => {
-    res.send("Express + TypeScript Server");
-});
-app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+app.use((0, cors_1.default)());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use(express_1.default.json());
+const PORT = process.env.PORT || 5000;
+app.use("/api/users", userRouter_1.default);
+const startServer = async () => {
+    try {
+        const response = await (0, config_1.connectDB)();
+        if (response) {
+            console.log("Connection successful");
+        }
+        app.listen(PORT, () => {
+            console.log("App listening on port 8081!");
+        });
+    }
+    catch (e) {
+        console.log(e);
+    }
+};
+startServer();
