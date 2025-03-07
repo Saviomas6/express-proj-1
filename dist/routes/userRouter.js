82a2027b7be2ebc77ae4c6512d9449605934061b
Savio Mascarenhas <savio.m@adsmn.in>
2025-03-07 20:02:44 +0530

    minor changes111555

---
+++ dist/routes/userRouter.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const handleUserSignUp_1 = require("../controller/handleUserSignUp");
+const handleUserSignIn_1 = require("../controller/handleUserSignIn");
+const handleUserDetails_1 = require("../controller/handleUserDetails");
+const userRouter = (0, express_1.Router)();
+userRouter.get("/", (req, res) => {
+    res.send("Hello Express");
+});
+userRouter.post("/signin", handleUserSignIn_1.handleUserSignIn);
+userRouter.post("/signup", handleUserSignUp_1.handleUserSignUp);
+userRouter.get("/userDetail", handleUserDetails_1.handleUserDetails);
+exports.default = userRouter;
+//# sourceMappingURL=userRouter.js.map
