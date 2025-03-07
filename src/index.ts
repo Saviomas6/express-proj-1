b13eb561a118e00e3ed7b2eacb55f7de0a690e58
Savio Mascarenhas <savio.m@adsmn.in>
2025-03-07 19:57:36 +0530

    minor changes1111333

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,16 @@
-import express from "express";
-import cors from "cors";
-import "dotenv/config";
-import { connectDB } from "./config/config";
-import userRouter from "./routes/userRouter";
+import express, { Request, Response } from "express";
 
 const app = express();
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const port = process.env.PORT || 8080;
 
-const PORT = process.env.PORT || 5000;
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Express Typescript on Vercel");
+});
 
-app.use(userRouter);
+app.get("/ping", (_req: Request, res: Response) => {
+  res.send("pong 🏓");
+});
 
-const startServer = async () => {
-  try {
-    const response = await connectDB();
-    if (response) {
-      console.log("Connection successful");
-    }
-    app.listen(PORT, () => {
-      console.log("App listening on port 8081!");
-    });
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-startServer();
+app.listen(port, () => {
+  console.log(`Server is listening on ${port}`);
+});
